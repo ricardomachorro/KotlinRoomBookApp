e0cd408e16f8e0efe297184b98629b2036a7e085
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2024-09-13 12:12:19 -0600

    Actualizacion primera version

--- app/src/main/java/com/example/bookorganizationapp/ui/main/view/NewBook/NewBookScreen.kt
+++ app/src/main/java/com/example/bookorganizationapp/ui/main/view/NewBook/NewBookScreen.kt
@@ -17,6 +17,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -39,6 +44,8 @@ fun NewBookScreen(drawerState: DrawerState, scope: CoroutineScope, state: BookSt
 
 @Composable
 fun formNewBook( state: BookState,onEvent: (BookEvent) -> Unit){
+    
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -136,7 +143,9 @@ fun formNewBook( state: BookState,onEvent: (BookEvent) -> Unit){
                     value = state.author,
                     onValueChange = {
                         onEvent(BookEvent.SetAuthor(it))
-                    })
+                    }
+
+                    )
 
 
                 Text(
