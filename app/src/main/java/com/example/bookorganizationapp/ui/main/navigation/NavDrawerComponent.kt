dcac1fac3500a95ac73bbb19a3b5c9dd895560e2
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2024-09-13 17:04:04 -0600

    Nuevo navigation drawer

--- app/src/main/java/com/example/bookorganizationapp/ui/main/navigation/NavDrawerComponent.kt
+++ app/src/main/java/com/example/bookorganizationapp/ui/main/navigation/NavDrawerComponent.kt
@@ -1,19 +1,32 @@
 package com.example.bookorganizationapp.ui.main.navigation
 
+import android.annotation.SuppressLint
+import android.view.Menu
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -26,6 +39,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.bookorganizationapp.data.internal.BookEvent
 import com.example.bookorganizationapp.data.internal.BookState
 import com.example.bookorganizationapp.data.internal.LibraryState
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 
@@ -83,4 +97,52 @@ fun NavDrawerComponent(state: BookState, libraryState: LibraryState, onEvent: (B
         NavigationDrawerRoutes(navController = navHostController , drawerState = drawerState , scope = scope,state=state,library =libraryState,onEvent=onEvent)
 
     }
+}
+
+@Composable
+fun MainScreen(state: BookState, libraryState: LibraryState, onEvent: (BookEvent) -> Unit){
+
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    Scaffold (
+        topBar = {CostumeTopBar(scope,drawerState)},
+
+
+    ){
+            innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+
+            ) {
+
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CostumeTopBar(
+    scope: CoroutineScope,
+    drawerState: DrawerState
+){
+
+    TopAppBar(
+        title = {Text("Organization App")},
+        navigationIcon = {
+            IconButton(onClick = {
+                scope.launch {
+                    drawerState.open()
+                }
+            }) {
+
+                Icon(imageVector =  Icons.Filled.Menu,
+                    contentDescription = "")
+            }
+        }
+    )
+    
 }
--- app/src/main/java/com/example/bookorganizationapp/ui/main/root/RootComponent.kt
+++ app/src/main/java/com/example/bookorganizationapp/ui/main/root/RootComponent.kt
@@ -4,10 +4,12 @@ import androidx.compose.runtime.Composable
 import com.example.bookorganizationapp.data.internal.BookEvent
 import com.example.bookorganizationapp.data.internal.BookState
 import com.example.bookorganizationapp.data.internal.LibraryState
+import com.example.bookorganizationapp.ui.main.navigation.MainScreen
 import com.example.bookorganizationapp.ui.main.navigation.NavDrawerComponent
 
 
 @Composable
 fun RootComponet(state: BookState,libraryState: LibraryState, onEvent:(BookEvent) -> Unit){
-    NavDrawerComponent(state,libraryState,onEvent)
+    //NavDrawerComponent(state,libraryState,onEvent)
+    MainScreen(state = state, libraryState = libraryState, onEvent = onEvent )
 }
