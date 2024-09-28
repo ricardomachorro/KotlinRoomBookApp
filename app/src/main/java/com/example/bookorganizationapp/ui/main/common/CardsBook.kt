b6b2e2088c1830f01106bbf24dcf60196dfa2981
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2024-09-27 18:25:23 -0600

    Correccion diseño elemento card de libros

--- app/src/main/java/com/example/bookorganizationapp/ui/main/common/CardsBook.kt
+++ app/src/main/java/com/example/bookorganizationapp/ui/main/common/CardsBook.kt
@@ -1,10 +1,12 @@
 package com.example.bookorganizationapp.ui.main.common
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
@@ -21,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
@@ -40,11 +43,133 @@ fun CardBook(navController: NavController,bookObject:Book,onEvent: (BookEvent) -
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 15.dp, vertical = 5.dp),
-        colors = CardDefaults.cardColors(
+       /* colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.secondary
-        )
+        )*/
     ){
 
+
+       Column(
+           modifier = Modifier
+               .fillMaxWidth()
+       ){
+
+           Row(
+               modifier = Modifier
+                   .fillMaxWidth()
+                   .padding(horizontal = 20.dp),
+               horizontalArrangement = Arrangement.SpaceBetween,
+               verticalAlignment = Alignment.CenterVertically
+           ){
+               Column {
+                   Text(
+                       text = bookObject.name,
+                       fontSize = 25.sp,
+                       fontWeight = FontWeight.ExtraBold
+                   )
+
+                   Text(
+                       text = bookObject.author,
+                       fontSize = 13.sp,
+                       fontWeight = FontWeight.Light,
+                       fontFamily = FontFamily.Monospace
+                   )
+               }
+
+               Row(
+                   verticalAlignment = Alignment.CenterVertically
+               ){
+
+
+                   IconButton(
+                       onClick = {
+                           onEvent(BookEvent.SetId(bookObject.id))
+                           onEvent(BookEvent.SetTitle(bookObject.name))
+                           onEvent(BookEvent.SetAuthor(bookObject.author))
+                           onEvent(BookEvent.SetCategory(bookObject.category))
+                           onEvent(BookEvent.SetPages(bookObject.last_page_read))
+                           onEvent(BookEvent.SetStartedDate(bookObject.first_read))
+                           onEvent(BookEvent.SetUpdatedDate(bookObject.last_read))
+                           onEvent(BookEvent.SetBookCompleted(bookObject.finished))
+                           navController.navigate("BookModification")
+
+                       }) {
+                       Icon(imageVector = Icons.Default.Edit,contentDescription = "")
+                   }
+
+
+                   IconButton(
+                       onClick = {
+                           onEvent(BookEvent.DeleteBook(bookObject))
+                       }) {
+                       Icon(imageVector = Icons.Default.Delete,contentDescription = "")
+                   }
+
+                   Checkbox(
+                       checked = (bookObject.finished == true),
+                       onCheckedChange = {
+                           onEvent(BookEvent.SetId(bookObject.id))
+                           onEvent(BookEvent.SetTitle(bookObject.name))
+                           onEvent(BookEvent.SetAuthor(bookObject.author))
+                           onEvent(BookEvent.SetCategory(bookObject.category))
+                           onEvent(BookEvent.SetPages(bookObject.last_page_read))
+                           onEvent(BookEvent.SetStartedDate(bookObject.first_read))
+                           onEvent(BookEvent.SetUpdatedDate(bookObject.last_read))
+                           onEvent(BookEvent.SetBookCompleted(!bookObject.finished))
+                           onEvent(BookEvent.SaveBook)
+                       }
+                   )
+
+               }
+           }
+
+           Row(
+               modifier = Modifier
+                   .fillMaxWidth()
+                   .padding(horizontal =20.dp,vertical = 5.dp),
+               horizontalArrangement = Arrangement.SpaceAround
+           ){
+
+               Text(
+                   text="F.Actual:"+calendarTool.CalendarToString(bookObject.last_read)
+               )
+
+
+
+               Text(
+                   text="F.Inicio:"+calendarTool.CalendarToString(bookObject.first_read)
+               )
+
+
+
+           }
+
+           Row(
+               modifier = Modifier
+                   .fillMaxWidth()
+                   .padding(horizontal =20.dp,vertical = 5.dp),
+               horizontalArrangement = Arrangement.SpaceAround
+           ){
+
+               Text(
+                   text="Categoria:"+ bookObject.category
+               )
+
+
+
+               Text(
+                   text="Pagina:"+bookObject.last_page_read.toString()
+               )
+
+
+
+           }
+
+
+
+       }
+
+        /*
 
       Column(
            modifier = Modifier
@@ -53,19 +178,23 @@ fun CardBook(navController: NavController,bookObject:Book,onEvent: (BookEvent) -
        ){
 
 
+
            Row(
                modifier = Modifier
                    .fillMaxWidth(),
-               horizontalArrangement = Arrangement.SpaceBetween
+               horizontalArrangement = Arrangement.SpaceBetween,
+
 
            ){
+
+
 
                Text(
                    buildAnnotatedString {
                        withStyle(
                            style = SpanStyle(
                                fontSize = 25.sp,
-                               fontWeight = FontWeight.Bold
+                               fontWeight = FontWeight.ExtraBold
                            )
                        ) {
                            append(bookObject.name)
@@ -74,7 +203,7 @@ fun CardBook(navController: NavController,bookObject:Book,onEvent: (BookEvent) -
                        withStyle(
                            style = SpanStyle(
                                fontSize =  15.sp,
-                               fontWeight = FontWeight.SemiBold
+                               fontWeight = FontWeight.Light
                            )
                        ){
                            append(" - "+bookObject.author)
@@ -198,7 +327,7 @@ fun CardBook(navController: NavController,bookObject:Book,onEvent: (BookEvent) -
 
        }
 
-
+        */
     }
 
 }
