5bc3089fe35c105b3c7330b5c472c1eb905d10a4
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2024-10-08 15:24:45 -0600

    Animacion lista

--- app/src/main/java/com/example/bookorganizationapp/ui/main/common/CardsBook.kt
+++ app/src/main/java/com/example/bookorganizationapp/ui/main/common/CardsBook.kt
@@ -1,5 +1,12 @@
 package com.example.bookorganizationapp.ui.main.common
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -19,6 +26,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.SpanStyle
@@ -37,297 +49,164 @@ import com.example.bookorganizationapp.util.CalendarTools
 @Composable
 fun CardBook(navController: NavController,bookObject:Book,onEvent: (BookEvent) -> Unit){
 
-    val calendarTool: CalendarTools= CalendarTools()
 
-    Card (
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 15.dp, vertical = 5.dp),
-       /* colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.secondary
-        )*/
-    ){
+    var isVisible by remember { mutableStateOf(false ) }
 
-
-       Column(
-           modifier = Modifier
-               .fillMaxWidth()
-       ){
-
-           Row(
-               modifier = Modifier
-                   .fillMaxWidth()
-                   .padding(horizontal = 20.dp),
-               horizontalArrangement = Arrangement.SpaceBetween,
-               verticalAlignment = Alignment.CenterVertically
-           ){
-               Column {
-                   Text(
-                       text = bookObject.name,
-                       fontSize = 25.sp,
-                       fontWeight = FontWeight.ExtraBold
-                   )
-
-                   Text(
-                       text = bookObject.author,
-                       fontSize = 13.sp,
-                       fontWeight = FontWeight.Light,
-                       fontFamily = FontFamily.Monospace
-                   )
-               }
-
-               Row(
-                   verticalAlignment = Alignment.CenterVertically
-               ){
-
-
-                   IconButton(
-                       onClick = {
-                           onEvent(BookEvent.SetId(bookObject.id))
-                           onEvent(BookEvent.SetTitle(bookObject.name))
-                           onEvent(BookEvent.SetAuthor(bookObject.author))
-                           onEvent(BookEvent.SetCategory(bookObject.category))
-                           onEvent(BookEvent.SetPages(bookObject.last_page_read))
-                           onEvent(BookEvent.SetStartedDate(bookObject.first_read))
-                           onEvent(BookEvent.SetUpdatedDate(bookObject.last_read))
-                           onEvent(BookEvent.SetBookCompleted(bookObject.finished))
-                           navController.navigate("BookModification")
-
-                       }) {
-                       Icon(imageVector = Icons.Default.Edit,contentDescription = "")
-                   }
-
-
-                   IconButton(
-                       onClick = {
-                           onEvent(BookEvent.DeleteBook(bookObject))
-                       }) {
-                       Icon(imageVector = Icons.Default.Delete,contentDescription = "")
-                   }
-
-                   Checkbox(
-                       checked = (bookObject.finished == true),
-                       onCheckedChange = {
-                           onEvent(BookEvent.SetId(bookObject.id))
-                           onEvent(BookEvent.SetTitle(bookObject.name))
-                           onEvent(BookEvent.SetAuthor(bookObject.author))
-                           onEvent(BookEvent.SetCategory(bookObject.category))
-                           onEvent(BookEvent.SetPages(bookObject.last_page_read))
-                           onEvent(BookEvent.SetStartedDate(bookObject.first_read))
-                           onEvent(BookEvent.SetUpdatedDate(bookObject.last_read))
-                           onEvent(BookEvent.SetBookCompleted(!bookObject.finished))
-                           onEvent(BookEvent.SaveBook)
-                       }
-                   )
-
-               }
-           }
-
-           Row(
-               modifier = Modifier
-                   .fillMaxWidth()
-                   .padding(horizontal =20.dp,vertical = 5.dp),
-               horizontalArrangement = Arrangement.SpaceAround
-           ){
-
-               Text(
-                   text="F.Actual:"+calendarTool.CalendarToString(bookObject.last_read)
-               )
-
-
-
-               Text(
-                   text="F.Inicio:"+calendarTool.CalendarToString(bookObject.first_read)
-               )
-
-
-
-           }
-
-           Row(
-               modifier = Modifier
-                   .fillMaxWidth()
-                   .padding(horizontal =20.dp,vertical = 5.dp),
-               horizontalArrangement = Arrangement.SpaceAround
-           ){
-
-               Text(
-                   text="Categoria:"+ bookObject.category
-               )
-
-
-
-               Text(
-                   text="Pagina:"+bookObject.last_page_read.toString()
-               )
-
-
-
-           }
-
-
-
-       }
-
-        /*
-
-      Column(
-           modifier = Modifier
-               .fillMaxWidth()
-               .padding(15.dp)
-       ){
-
-
-
-           Row(
-               modifier = Modifier
-                   .fillMaxWidth(),
-               horizontalArrangement = Arrangement.SpaceBetween,
-
-
-           ){
-
-
-
-               Text(
-                   buildAnnotatedString {
-                       withStyle(
-                           style = SpanStyle(
-                               fontSize = 25.sp,
-                               fontWeight = FontWeight.ExtraBold
-                           )
-                       ) {
-                           append(bookObject.name)
-                       }
-
-                       withStyle(
-                           style = SpanStyle(
-                               fontSize =  15.sp,
-                               fontWeight = FontWeight.Light
-                           )
-                       ){
-                           append(" - "+bookObject.author)
-                       }
-                   }
-               )
-
-               Row(){
-                   Column(){
-                       Text(
-                           "Finalizdo"
-                       )
-                       Checkbox(
-                           checked = (bookObject.finished == true),
-                           onCheckedChange = {
-                               onEvent(BookEvent.SetId(bookObject.id))
-                               onEvent(BookEvent.SetTitle(bookObject.name))
-                               onEvent(BookEvent.SetAuthor(bookObject.author))
-                               onEvent(BookEvent.SetCategory(bookObject.category))
-                               onEvent(BookEvent.SetPages(bookObject.last_page_read))
-                               onEvent(BookEvent.SetStartedDate(bookObject.first_read))
-                               onEvent(BookEvent.SetUpdatedDate(bookObject.last_read))
-                               onEvent(BookEvent.SetBookCompleted(!bookObject.finished))
-                               onEvent(BookEvent.SaveBook)
-                           }
-                       )
-                   }
-               }
-
-
-
-
-           }
-
-          Row(
-              modifier = Modifier
-                  .fillMaxWidth(),
-              horizontalArrangement = Arrangement.Start
-
-          ){
-
-              Text(bookObject.category)
-          }
-
-
-
-           Row(
-               modifier = Modifier
-                   .fillMaxWidth()
-                   .padding(vertical = 10.dp),
-               horizontalArrangement = Arrangement.SpaceBetween
-           ){
-
-
-               Column(){
-
-
-                   Text(
-                       text="F.Actual:"+calendarTool.CalendarToString(bookObject.last_read)
-                   )
-
-
-
-                   Text(
-                       text="F.Inicio:"+calendarTool.CalendarToString(bookObject.first_read)
-                   )
-
-               }
-
-
-
-               Column(){
-
-
-                   Text(
-                       text="Pagina:"+bookObject.last_page_read.toString()
-                   )
-
-                   Text(
-                       text = if(bookObject.finished)  "finalizado" else "no finalizado"
-                   )
-
-               }
-
-               Column(){
-
-                   IconButton(
-                       onClick = {
-                           onEvent(BookEvent.SetId(bookObject.id))
-                           onEvent(BookEvent.SetTitle(bookObject.name))
-                           onEvent(BookEvent.SetAuthor(bookObject.author))
-                           onEvent(BookEvent.SetCategory(bookObject.category))
-                           onEvent(BookEvent.SetPages(bookObject.last_page_read))
-                           onEvent(BookEvent.SetStartedDate(bookObject.first_read))
-                           onEvent(BookEvent.SetUpdatedDate(bookObject.last_read))
-                           onEvent(BookEvent.SetBookCompleted(bookObject.finished))
-                           navController.navigate("BookModification")
-
-                       }) {
-                       Icon(imageVector = Icons.Default.Edit,contentDescription = "")
-                   }
-
-                   
-                   IconButton(
-                       onClick = {
-                           onEvent(BookEvent.DeleteBook(bookObject))
-                       }) {
-                       Icon(imageVector = Icons.Default.Delete,contentDescription = "")
-                   }
-
-                  
-                   
-                   
-               }
-
-
-
-           }
-
-
-
-       }
-
-        */
+    LaunchedEffect(Unit) {
+        isVisible = true
     }
 
+
+    val calendarTool: CalendarTools= CalendarTools()
+
+    val bgColor by animateColorAsState(
+        targetValue = if (!isVisible)
+            MaterialTheme.colorScheme.background else MaterialTheme.colorScheme.primary, label = ""
+        , animationSpec = tween(600)
+    )
+
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = fadeIn(
+            animationSpec = tween(500)
+        ) + scaleIn(
+            animationSpec = tween(500)
+        ),
+        exit = fadeOut(
+            animationSpec = tween(500)
+        ) + scaleOut(
+            animationSpec = tween(500)
+        )
+    ) {
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 15.dp, vertical = 5.dp),
+             colors = CardDefaults.cardColors(
+            containerColor = bgColor
+            )
+        ) {
+
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column {
+                        Text(
+                            text = bookObject.name,
+                            fontSize = 25.sp,
+                            fontWeight = FontWeight.ExtraBold
+                        )
+
+                        Text(
+                            text = bookObject.author,
+                            fontSize = 13.sp,
+                            fontWeight = FontWeight.Light,
+                            fontFamily = FontFamily.Monospace
+                        )
+                    }
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+
+
+                        IconButton(
+                            onClick = {
+                                onEvent(BookEvent.SetId(bookObject.id))
+                                onEvent(BookEvent.SetTitle(bookObject.name))
+                                onEvent(BookEvent.SetAuthor(bookObject.author))
+                                onEvent(BookEvent.SetCategory(bookObject.category))
+                                onEvent(BookEvent.SetPages(bookObject.last_page_read))
+                                onEvent(BookEvent.SetStartedDate(bookObject.first_read))
+                                onEvent(BookEvent.SetUpdatedDate(bookObject.last_read))
+                                onEvent(BookEvent.SetBookCompleted(bookObject.finished))
+                                navController.navigate("BookModification")
+
+                            }) {
+                            Icon(imageVector = Icons.Default.Edit, contentDescription = "")
+                        }
+
+
+                        IconButton(
+                            onClick = {
+                                onEvent(BookEvent.DeleteBook(bookObject))
+                            }) {
+                            Icon(imageVector = Icons.Default.Delete, contentDescription = "")
+                        }
+
+                        Checkbox(
+                            checked = (bookObject.finished == true),
+                            onCheckedChange = {
+                                onEvent(BookEvent.SetId(bookObject.id))
+                                onEvent(BookEvent.SetTitle(bookObject.name))
+                                onEvent(BookEvent.SetAuthor(bookObject.author))
+                                onEvent(BookEvent.SetCategory(bookObject.category))
+                                onEvent(BookEvent.SetPages(bookObject.last_page_read))
+                                onEvent(BookEvent.SetStartedDate(bookObject.first_read))
+                                onEvent(BookEvent.SetUpdatedDate(bookObject.last_read))
+                                onEvent(BookEvent.SetBookCompleted(!bookObject.finished))
+                                onEvent(BookEvent.SaveBook)
+                            }
+                        )
+
+                    }
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp, vertical = 5.dp),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+
+                    Text(
+                        text = "F.Actual:" + calendarTool.CalendarToString(bookObject.last_read)
+                    )
+
+
+
+                    Text(
+                        text = "F.Inicio:" + calendarTool.CalendarToString(bookObject.first_read)
+                    )
+
+
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp, vertical = 5.dp),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+
+                    Text(
+                        text = "Categoria:" + bookObject.category
+                    )
+
+
+
+                    Text(
+                        text = "Pagina:" + bookObject.last_page_read.toString()
+                    )
+
+
+                }
+
+
+            }
+
+
+        }
+    }
 }
